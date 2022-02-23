255dbbfaa3390b3bfbed9418e369918466456ea6
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2022-02-23 13:35:20 +0530

    imagesAdded

--- src/Component/NavbarComponent.js
+++ src/Component/NavbarComponent.js
@@ -9,8 +9,8 @@ const NavbarComponent = () => {
           <Navbar.Brand href="/"><img src="/images/icon.png" style={{ width: "45px", borderRadius: "8px" }}></img></Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/" style={{ color: "#f28e23", fontWeight: "bold" }}>Home</Nav.Link>
-              <Nav.Link href="/profile" style={{ color: "#f28e23", fontWeight: "bold" }}>Profile</Nav.Link>
+              <Nav.Link href="/" style={{ color: "#f28e23", fontWeight: "bold" }}><img src="/images/Home.png" style={{ width: "25px", borderRadius: "8px", paddingRight: "5px" }}></img>Home</Nav.Link>
+              <Nav.Link href="/profile" style={{ color: "#f28e23", fontWeight: "bold" }}><img src="/images/profile.png" style={{ width: "25px", borderRadius: "8px", paddingRight: "5px" }}></img>Profile</Nav.Link>
             </Nav>
           </Navbar.Collapse>
 
